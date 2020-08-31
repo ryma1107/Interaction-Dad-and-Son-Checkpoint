bf9f8c9aa2a31f92884222e3b2682c6641029975
ryma1107 <jebrirym91@gmail.com>
2020-08-31 17:28:54 +0100

    last commit

---
+++ src/app/fils/fils.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fils',
+  templateUrl: './fils.component.html',
+  styleUrls: ['./fils.component.css']
+})
+export class FilsComponent implements OnInit {
+
+  @Input() colorPere: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
